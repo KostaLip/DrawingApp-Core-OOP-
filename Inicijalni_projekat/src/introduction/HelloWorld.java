71022b2d7990edea08de6bbcd771d766a0fe7b89
hp <hp@DESKTOP-I64SCUM>
2022-03-16 16:40:30 +0100

    vajda radi

--- Inicijalni_projekat/src/introduction/HelloWorld.java
+++ Inicijalni_projekat/src/introduction/HelloWorld.java
@@ -4,9 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 12;
 		System.out.println("Hello, World!");
 		System.out.println("kum");
-
+		System.out.println(a);
 	}
 
 }
